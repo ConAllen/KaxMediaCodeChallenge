cf549cb92c53fcbcbb06958ff6b2fc04b64f64c0
Your Name <you@example.com>
2018-03-10 16:41:29 +0000

    slick slider

--- resources/js/script.js
+++ resources/js/script.js
@@ -2,6 +2,50 @@
 $(document).ready(function() {
 
 
+    $(document).ready(function() {
+
+      $(".your-class").slick({
+        // dots: true,
+        arrows:true,
+        infinite: true,
+        centerMode: true,
+        slidesToShow: 5,
+        slidesToScroll: 5,
+        autoplay:true,
+        autoplaySpeed: 3000,
+        slidesToShow: 10,
+        responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+      });
+
+
+});
 
 
 /*
